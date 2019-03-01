a348714b050cd2af75f1d43c3339a920e3d5fbf6
BernouWagenaar <Bernou.Wagenaar@AvivaSolutions.nl>
2019-03-01 22:55:19 +0100

    Add project files.

---
+++ MedioClinic/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using CMS;
+
+[assembly: AssemblyDiscoverable]
+
+[assembly: AssemblyTitle("MedioClinic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("115913fb-99c1-4e6d-be1e-4cd75e693d94")]
+
+[assembly: AssemblyProduct("MedioClinic")]
+[assembly: AssemblyCopyright("© 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("12.0.0.0")]
+[assembly: AssemblyFileVersion("12.0.6900.17470")]
+[assembly: AssemblyInformationalVersion("12.0.0")]
